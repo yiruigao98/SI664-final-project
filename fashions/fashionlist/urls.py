1246a4a0b5b1c886ece07d032fdcb40cb6df493d
Yirui Gao <yiruigao@umich.edu>
2019-12-01 01:13:29 -0500

    Navigation bar

---
+++ fashions/fashionlist/urls.py
@@ -0,0 +1,44 @@
+"""samples URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', include('fashions.urls')),  # Keep
+    path('admin/', admin.site.urls),  # Keep
+    path('accounts/', include('django.contrib.auth.urls')),  # Keep
+    url(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
+]
+
+# Serve the favicon
+import os
+from django.views.static import serve
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    path('KJfavicon.ico', serve, {
+            'path': 'KJfavicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
+]
+
+# References
+
+# https://docs.djangoproject.com/en/2.2/ref/urls/#include
+
